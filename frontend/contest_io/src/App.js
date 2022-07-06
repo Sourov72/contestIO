5861bc8f34c4613262b9119c8fc1c2e51caa8fd6
Sourov72 <sourov.jph@gmail.com>
2022-07-06 15:02:19 +0600

    "frontend added"

---
+++ frontend/contest_io/src/App.js
@@ -0,0 +1,37 @@
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar.component"
+// import ExerciseList from "./components/exercise-list.component"
+// import EditExercise from "./components/edit-exercise.component"
+// import CreateExercise from "./components/create-exercise.component"
+// import CreateUser from "./components/create-user.component"
+
+// import Home from "./components/home"
+
+
+function App() {
+  return (
+    <>
+      {
+        <Router>
+          <div className="container">
+
+
+            <Navbar />
+            {/* <Routes>
+              
+              <Route path="/" element={<ExerciseList />} />
+              <Route path="/edit/:id" element={<EditExercise />} />
+              <Route path="/create" element={<CreateExercise />} />
+              <Route path="/user" element={<CreateUser />} />
+
+            </Routes> */}
+
+          </div>
+        </Router>
+      }
+    </>
+  );
+}
+
+export default App;
