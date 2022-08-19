f2aeeb3940808f10034f550f775f4b6fa5f9ade0
Kamrujjaman <kamrujjaman05081@gmail.com>
2022-08-19 07:49:26 +0600

    refactored

--- frontend/contest_io/src/components/contests/contestBox.js
+++ frontend/contest_io/src/components/contests/contestBox.js
@@ -2,9 +2,7 @@ import React from "react";
 import { ContestCard } from "./contestCard";
 import { Link } from "react-router-dom";
 
-
-
-export const ContestBox = ({ contests, boxTitle, q, col }) => {
+export const ContestBox = ({contests, boxTitle, q, col}) => {
   var fol=1;
   function myfollowClick(){
     fol=1-fol;
@@ -13,7 +11,7 @@ export const ContestBox = ({ contests, boxTitle, q, col }) => {
     
     <div className="row py-2 mb-4 contest-box card-group">
       <h4 className="text-center fw-bold">{boxTitle}</h4>
-      {contests.length > 0 ? (
+      {contests.length > 0 ? 
         <>
         {contests.map((contest) => (
         <div>
@@ -33,34 +31,9 @@ export const ContestBox = ({ contests, boxTitle, q, col }) => {
         <Link className="d-flex justify-content-center" to="/contests/search" state={{query : q}}>
           <button className="btn btn-danger px-4">View More</button>
         </Link>}
-          {console.log("cotn", contests[0])}
-          {contests.map((contest) => {
-            {
-              if (contest !== null) {
-                {
-                  console.log("helooooooooo", contest);
-                }
-                return (
-                  <ContestCard key={contest._id} contest={contest} col={col} />
-                );
-              }
-            }
-          })}
-          {col == 12 && (
-            <Link
-              className="d-flex justify-content-center"
-              to="/contests/search"
-              state={{ query: q }}
-            >
-              <button className="btn btn-danger px-4">View More</button>
-            </Link>
-          )}
         </>
-      ) : (
-        <div className="text-center fw-light fst-italic text-muted">
-          no contests to show
-        </div>
-      )}
+
+        : <div className="text-center fw-light fst-italic text-muted">no contests to show</div>}
     </div>
   );
 };
