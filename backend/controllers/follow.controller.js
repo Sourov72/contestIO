cb1a1c514652c8b8e56c29db8e5f27dbecd38e2c
Kamrujjaman <kamrujjaman05081@gmail.com>
2022-08-27 07:18:39 +0600

    1

--- backend/controllers/follow.controller.js
+++ backend/controllers/follow.controller.js
@@ -1,12 +1,13 @@
 const FollowModel = require("../models/contestFollow.model");
 const userModel = require("../models/user.model");
+const ContestModel = require("../models/contest.model");
 const mongoose = require("mongoose");
 
 // get all participants
 const getFollowList = async (req, res) => {
     const { id } = req.params;
-    const contestL = await FollowModel.find({"contestID":id});
-    res.status(200).json(contestL);
+    const userList = await FollowModel.find({"contestID":id});
+    res.status(200).json(userList);
 };
 
 const createFollow = async (req, res) => {
@@ -21,10 +22,32 @@ const unfollow =async(req,res) => {
     const contestID = req.body.contestID;
     const userID =req.body.userID;
     await FollowModel.findOneAndDelete({"userID":userID,"contestID":contestID});
-    res.status(200).json("Unollowed");
+    res.status(200).json("Unfollowed");
 }
 
+const getUserFollowList = async (req, res) => {
+    const { userId } = req.params;
+    const contestList = await FollowModel.find({"userID":userId},{"contestID":1});
+    res.status(200).json(contestList);
+};
+
 const getNotification=async(req,res)=>{
+    const { userId } = req.params;
+    const contestList = await FollowModel.find({"userID":userId},{"contestID":1});
+    const strRet={};
+
+    for (let i=0;i<contestList.length;i++){
+        console.log("contetIDs",contestList[i]);
+        const dt=new Date();
+        const temp=await ContestModel.find({"contestID":contestList[i],"registrationEndTime":"$lte:dt"},{"contestID":1,"registrationEndTime":"1"});
+        strRet.push(temp);
+
+        //get notifications
+    }
+    
+    if(contestList.length<1){
+        res.status(200).join("")
+    }
     res.status(200).json("Notify1");
 }
 
